b4fbaa6093e04968895f2522c82ddb3ebced0067
kavithayambadi <108254552+kavithayambadi@users.noreply.github.com>
2022-07-14 14:04:06 +0530

    Add files via upload

---
+++ HighestNumber.java
@@ -0,0 +1,65 @@
+/* JFM1T4_Assignment1:
+   Write a program to find the highest among the given 3 numbers.
+   Prompt the user for the 3 numbers to be input from the terminal.
+
+   SampleInput: 
+   Enter the first number: 11
+   Enter the second number: 22
+   Enter the third number: 1
+
+   Expected Output:
+   22 is highest number
+ 	
+*/
+
+//import statements for java program to read inputs using Scanner class 
+
+     
+//Add main method
+
+//Declare the three variables
+
+//Use the scanner class to provide input at execution time
+/* 
+ Scanner s= new Scanner(System.in);
+ System.out.println("Enter the first number");
+ number1=s.nextInt();
+ 
+*/
+
+//check which number is highest using if else condition
+
+//Print the highest of three numbers
+ 
+
+
+import java.util.Scanner;
+public class 111 
+{
+    public static void main(String[] args) 
+    {
+        int x, y, z;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        x = s.nextInt();
+        System.out.print("Enter the second number:");
+        y = s.nextInt();
+        System.out.print("Enter the third number:");
+        z = s.nextInt();
+        if(x > y && x > z)
+        {
+            System.out.println("Largest number is:"+x);
+        }
+        else if(y > z)
+        {
+            System.out.println("Largest number is:"+y);
+        }
+        else
+        {
+            System.out.println("Largest number is:"+z);
+        }
+ 
+    }
+}
+
+
