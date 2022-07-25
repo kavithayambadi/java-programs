e3532aba62643d3a392e30cd7f7aa99324875f72
kavithayambadi <108254552+kavithayambadi@users.noreply.github.com>
2022-07-25 17:46:38 +0530

    Add files via upload

---
+++ Concat.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Concat{
+    public static void main(String[] args)
+  {
+      
+     Scanner s    = new Scanner(System.in);
+     System.out.println("Enter a string:");
+        String a=s.nextLine();
+    System.out.println("Enter a string:");
+        String b=s.nextLine();
+     System.out.println(a+b);
+  }
+}
